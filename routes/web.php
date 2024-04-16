e13947edcb77a1ff7a6fa7a93c08b8ba528bf902
Elton Fonseca <elton869@gmail.com>
2024-04-16 15:19:46 -0300

    07 - Usando o service container

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
+app()->bind('Exemplo', function() {
+    return new \App\Exemplo;
+});
+
 Route::get('/', function () {
-    return view('welcome');
+    $instanciaExemplo = app()->make('Exemplo');
+
+    dd($instanciaExemplo);
+    
+    
+    //return view('welcome');
 });
