cb1d58ba03507d2106b579fbc1d3167f4c32e5ab
Elton Fonseca <elton869@gmail.com>
2024-04-18 18:01:11 -0300

    19 - Real time facades

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,29 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\ConsultaCEPInterface;
-use Illuminate\View\Factory;
-use Illuminate\Http\Request;
-use Illuminate\Session\SessionManager;
-use Illuminate\Support\Facades\Session;
-use Illuminate\Support\Facades\View;
+use Facades\App\ConsultaCEPInterface;
 
 class HomeController extends Controller
 {
-    public function index(SessionManager $sessionManager, Factory $view)
+    public function index()
     {
-        session()->put('test', 'treinaweb1234 ');
-        echo session('test');
-
-        Session::put('test', 'treinaweb');
-        echo Session::get('test');
-
-        $sessionManager->put('test', 'treinaweb');
-        echo $sessionManager->get('test');
-
-        return view('welcome');
-        //return $view->make('welcome');
-
-        // $consultaCEP->consulta('12345678');
+        ConsultaCEPInterface::consulta('12345678');
     }
 }
