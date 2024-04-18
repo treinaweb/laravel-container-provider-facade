84c27b2d947340e3d753966e461a525234095be6
Elton Fonseca <elton869@gmail.com>
2024-04-18 17:42:19 -0300

    18 - Como as facades funcionam

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,19 +3,27 @@
 namespace App\Http\Controllers;
 
 use App\ConsultaCEPInterface;
-use App\Exemplo;
+use Illuminate\View\Factory;
 use Illuminate\Http\Request;
+use Illuminate\Session\SessionManager;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\View;
 
 class HomeController extends Controller
 {
-    public function index(ConsultaCEPInterface $consultaCEP)
+    public function index(SessionManager $sessionManager, Factory $view)
     {
+        session()->put('test', 'treinaweb1234 ');
+        echo session('test');
+
         Session::put('test', 'treinaweb');
         echo Session::get('test');
 
-        return View::make('welcome');
+        $sessionManager->put('test', 'treinaweb');
+        echo $sessionManager->get('test');
+
+        return view('welcome');
+        //return $view->make('welcome');
 
         // $consultaCEP->consulta('12345678');
     }
