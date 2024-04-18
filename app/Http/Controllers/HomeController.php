0b726e9b44a0c4a87cae8c518a1bb12e91064954
Elton Fonseca <elton869@gmail.com>
2024-04-18 17:20:20 -0300

    17 - Conhecendo as Facades

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,11 +5,18 @@ namespace App\Http\Controllers;
 use App\ConsultaCEPInterface;
 use App\Exemplo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\View;
 
 class HomeController extends Controller
 {
     public function index(ConsultaCEPInterface $consultaCEP)
     {
-        $consultaCEP->consulta('12345678');
+        Session::put('test', 'treinaweb');
+        echo Session::get('test');
+
+        return View::make('welcome');
+
+        // $consultaCEP->consulta('12345678');
     }
 }
